1d08fec79988b6b17667b86b6f970e13d4c970e7
tracer-ics2020 <tracer@njuics.org>
2021-05-21 17:31:02 +0800

    >  compile 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: e94513c0a0fc423cbc791d78e5c13294   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  17:31:02 up  6:36,  1 user,  load average: 0.96, 0.45, 0.30

--- nemu/src/isa/riscv64/intr.c
+++ nemu/src/isa/riscv64/intr.c
@@ -6,7 +6,10 @@ void raise_intr(DecodeExecState *s, word_t NO, vaddr_t epc) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+  reg_scr(SEPC_ID) = epc + 4;
+  reg_scr(SCAUSE_ID) = NO;
+
+  rtl_j(s, reg_scr(STVEC_ID));
 }
 
 void query_intr(DecodeExecState *s) {
