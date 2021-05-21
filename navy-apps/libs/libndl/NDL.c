bf637271ba5c558f1229dc986ffdd9fbd434da95
tracer-ics2020 <tracer@njuics.org>
2021-05-21 19:21:47 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: e94513c0a0fc423cbc791d78e5c13294   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  19:21:47 up  8:27,  1 user,  load average: 0.95, 0.76, 0.74

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -31,7 +31,7 @@ int NDL_PollEvent(char *buf, int len) {
 void NDL_OpenCanvas(int *w, int *h) {
   
   FILE* fp = fopen("/proc/dispinfo", "r");
-  char buf[128];
+  // char buf[128];
   // fread(buf, 128, 1, fp);
   // printf("buf: %s\n", buf);
   fscanf(fp, "WIDTH: %d\nHEIGHT: %d", &screen_w, &screen_h);
