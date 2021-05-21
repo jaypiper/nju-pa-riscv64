feb3b56d0575ab48b9eeca4dbb00f716b85b5590
tracer-ics2020 <tracer@njuics.org>
2021-05-21 21:08:28 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: e94513c0a0fc423cbc791d78e5c13294   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  21:08:28 up 10:13,  1 user,  load average: 0.81, 0.92, 0.86

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,6 +8,7 @@ size_t fs_read(int fd, void *buf, size_t len);
 size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
+void naive_uload(PCB *pcb, const char *filename);
 
 static int _sys_brk(int new_brk){
   // Log("in brk %lx\n", new_brk);
@@ -19,6 +20,12 @@ static int _sys_time(){
   // printf("sys time %ld\n", _timer.us);
   return _timer.us;
 }
+static int _sys_execve(char* filename, char* argv[], char* envp[]){
+  naive_uload(NULL, filename);
+  // context_uload(current, filename, argv, envp); //这里是不是还需要yield一下
+  // assert(0);
+  return 0;
+}
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -27,8 +34,7 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   switch (a[0]) {
-    case SYS_exit: halt(a[0]); break;
-    // case SYS_exit: _sys_execve("/bin/nterm", NULL, NULL); break;
+    case SYS_exit: _sys_execve("/bin/menu", NULL, NULL); break;
     case SYS_yield: yield(); break; 
     case SYS_open: c->GPRx = fs_open((char*)a[1], a[2], a[3]); break;
     case SYS_read: c->GPRx = fs_read(a[1], (void*)a[2], a[3]); break;
@@ -36,7 +42,7 @@ void do_syscall(Context *c) {
     case SYS_close: c->GPRx = fs_close(a[1]); break;
     case SYS_lseek: c->GPRx = fs_lseek(a[1], a[2], a[3]); break;
     case SYS_brk: c->GPRx = _sys_brk(a[1]); break;
-    // case SYS_execve: c->GPRx = _sys_execve((char*)a[1], (char**)a[2], (char**)a[3]); break;
+    case SYS_execve: c->GPRx = _sys_execve((char*)a[1], (char**)a[2], (char**)a[3]); break;
     case SYS_gettimeofday: c->GPRx = _sys_time(); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
