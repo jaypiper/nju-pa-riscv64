c808494b76b4579a9ebc80a3ed6c86066e8f4f51
tracer-ics2020 <tracer@njuics.org>
2021-05-21 19:22:27 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: e94513c0a0fc423cbc791d78e5c13294   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  19:22:27 up  8:27,  1 user,  load average: 0.99, 0.79, 0.75

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -49,13 +49,13 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   // yield();
   AM_GPU_CONFIG_T _config = io_read(AM_GPU_CONFIG);
   
-  int x = (offset/4) % _config.width;
-  int y = (offset/4) / _config.width;
+  int x = (offset) % _config.width;
+  int y = (offset) / _config.width;
   // printf("x: %d, y: %d len: %d offset: %d\n", x, y, len, offset);
   // int left = _config.width - (offset % _config.width);
   // size_t i = 0;
   assert(offset + len <= _config.width * _config.height * 4);
-  io_write(AM_GPU_FBDRAW, x, y, (void*)buf, len / 4, 1, true);
+  io_write(AM_GPU_FBDRAW, x, y, (void*)buf, len, 1, true);
   // const uint32_t* pixels = buf;
   // while(i < len){
   //   // _fbdraw.h = 1;
