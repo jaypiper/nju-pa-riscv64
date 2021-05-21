3e51bf1dc64e978da8d81f5f952e2bc1aa7945ba
tracer-ics2020 <tracer@njuics.org>
2021-05-21 13:46:37 +0800

    >  compile 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: e94513c0a0fc423cbc791d78e5c13294   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  13:46:37 up  2:51,  1 user,  load average: 0.88, 0.70, 0.65

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -17,27 +17,41 @@ static inline def_rtl(mv, rtlreg_t* dest, const rtlreg_t *src1) {
 
 static inline def_rtl(not, rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
-  TODO();
+  *dest = ~(*src1);
+  // TODO();
 }
 
 static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- -src1
-  TODO();
+  *dest = - (*src1);
+  // TODO();
 }
 
 static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
-  // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  if(width == 1) *dest = (sword_t)((int8_t)*src1);
+  else if(width == 2) *dest = (sword_t)((int16_t)*src1);
+  else if(width == 4) *dest = (sword_t)((int32_t)*src1);
+  else if(width == 8) *dest = *src1;
+  else assert(0);
 }
 
 static inline def_rtl(zext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- zeroext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  if(width == 1) *dest = (word_t)((uint8_t)*src1);
+  else if(width == 2) *dest = (word_t)((uint16_t)*src1);
+  else if(width == 4) *dest = (word_t)((uint32_t)*src1);
+  else if(width == 8) *dest = *src1;
 }
+
 
 static inline def_rtl(msb, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  TODO();
+  rtl_shri(s, t0, src1, width*8-1);
+  rtl_andi(s, dest, t0, 1);
+  // *dest = (*src1 >> (width*8-1)) & 1;
+  // TODO();
 }
+
+
 
 #endif
