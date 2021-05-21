db17691db1389a04d86903da051a6bf0cd556fab
tracer-ics2020 <tracer@njuics.org>
2021-05-21 17:41:01 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: e94513c0a0fc423cbc791d78e5c13294   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  17:41:01 up  6:46,  1 user,  load average: 0.62, 0.60, 0.46

--- abstract-machine/am/include/arch/riscv64-nemu.h
+++ abstract-machine/am/include/arch/riscv64-nemu.h
@@ -2,14 +2,14 @@
 #define ARCH_H__
 
 struct Context {
-  uintptr_t epc, cause, gpr[32], status;
+  uintptr_t gpr[32], cause, status, epc;
   void *pdir;
 };
 
 #define GPR1 gpr[17] // a7
-#define GPR2 gpr[0]
-#define GPR3 gpr[0]
-#define GPR4 gpr[0]
-#define GPRx gpr[0]
+#define GPR2 gpr[10]
+#define GPR3 gpr[11]
+#define GPR4 gpr[12]
+#define GPRx gpr[10]
 
 #endif
