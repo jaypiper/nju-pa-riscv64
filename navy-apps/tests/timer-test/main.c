d3a8d64942738bee6bdb8bbeeb41dea61408ce45
tracer-ics2020 <tracer@njuics.org>
2021-05-21 18:31:44 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: e94513c0a0fc423cbc791d78e5c13294   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  18:31:44 up  7:37,  1 user,  load average: 0.57, 0.65, 0.64

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -5,11 +5,12 @@ int main() {
 	NDL_Init(0);
 	int count = 0;
 	while (1) {
-		long long time = NDL_GetTicks();
-        	if(time / 500 >= count){
-    			count ++;
-    	 		printf("Hello from timer-test with count %d \n", count);
-        	}
+		struct timeval tv;	
+    long long time = gettimeofday(&tv, NULL);
+    if(tv.tv_usec / 500000 >= count){
+    	count ++;
+    	printf("Hello from timer-test with count %d \n", count);
+    }
         }
         NDL_Quit();
   /*
