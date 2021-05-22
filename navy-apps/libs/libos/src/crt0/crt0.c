1f2f5e6cdb4787c7559d9445cc89f37989605bc1
tracer-ics2020 <tracer@njuics.org>
2021-05-22 14:32:05 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: eacecca82bd04371b6cbff2c3952091f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  14:32:05 up  6:04,  1 user,  load average: 1.51, 1.30, 1.05

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,12 +1,26 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
+
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
-  exit(main(0, empty, empty));
+  int argc = *args;
+  char** argv = (char**)(args + 1);
+  char** envp = (char**)(args + 1 + argc + 1);
+
+  printf("argc: %d\n", argc);
+  for(int i = 0; i < argc; i++){
+    printf("argv: %d %s %p\n", i, argv[i], &argv[i]);
+  }
+  for(int j = 0; envp[j]; j++) printf("envp: %d %s\n", j, envp[j]);
+  printf("end\n");
+  
+  exit(main(argc, argv, envp));
+  // exit(main(0, empty, empty));
   assert(0);
 }
