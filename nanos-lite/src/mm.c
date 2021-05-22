66a1ee24b09937dc3898fc5392c06d628598c34d
tracer-ics2020 <tracer@njuics.org>
2021-05-22 13:59:38 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: eacecca82bd04371b6cbff2c3952091f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  13:59:38 up  5:31,  1 user,  load average: 1.47, 1.16, 0.83

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -3,7 +3,9 @@
 static void *pf = NULL;
 
 void* new_page(size_t nr_page) {
-  return NULL;
+  void* head = pf;
+  pf += nr_page * PGSIZE;
+  return head;
 }
 
 static inline void* pg_alloc(int n) {
