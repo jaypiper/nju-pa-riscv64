8ce33afe44897a2a17bdcfaaf43a08a73407a0bb
tracer-ics2020 <tracer@njuics.org>
2021-05-22 21:27:47 +0800

    >  compile 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: eacecca82bd04371b6cbff2c3952091f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  21:27:47 up 12:59,  1 user,  load average: 0.85, 1.11, 1.31

--- nemu/src/isa/riscv64/mmu.c
+++ nemu/src/isa/riscv64/mmu.c
@@ -13,7 +13,7 @@ paddr_t isa_mmu_translate(vaddr_t addr, int type, int len) {
   uint64_t* pg_base = (uint64_t*)((reg_scr(SATP_ID) & STAP_MASK) << 12);
   printf("base %lx\n", (uintptr_t)pg_base);
   uint64_t idx = (addr >> 30) & 0x1ff; 
-  printf("idx\n"); 
+  printf("idx: %d", idx == 0); 
   assert(pg_base[idx] & VALID_MASK);
 
   pg_base = (uint64_t*)pg_base[idx];
