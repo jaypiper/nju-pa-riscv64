64a13a2711ffe2715967414a9bf81aea38aa4482
tracer-ics2020 <tracer@njuics.org>
2021-05-23 12:56:45 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: 3383d3d361694b96806c5f00ad407121   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  12:56:45 up  5:07,  1 user,  load average: 0.41, 0.53, 0.54

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,14 +15,14 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  yield();
+  // yield();
   const char* _p = buf;
   for(int i = 0; i < len; i++) putch(*_p++);
   return len;
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  yield();
+  // yield();
   AM_INPUT_KEYBRD_T _keyboard = io_read(AM_INPUT_KEYBRD);
   if(_keyboard.keycode == AM_KEY_NONE) {
     sprintf(buf, "\n");
