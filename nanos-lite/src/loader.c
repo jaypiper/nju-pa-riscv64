f7571f31593445302ed094b2232cec2d612cd376
tracer-ics2020 <tracer@njuics.org>
2021-05-23 09:29:17 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: 3383d3d361694b96806c5f00ad407121   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  09:29:17 up  1:40,  1 user,  load average: 0.81, 0.95, 0.88

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -93,7 +93,7 @@ void context_uload(PCB* pcb, const char* filename, char *const argv[], char *con
   // pcb->as.area.end = (void*)((uint8_t*)pcb->stack + STACK_SIZE);
   void* _start = new_page(8);
   void* _end = (void*)((uint8_t*)_start + STACK_SIZE);
-  printf("as: %lx\n", (uintptr_t)(pcb->as.ptr));
+  // printf("as: %lx\n", (uintptr_t)(pcb->as.ptr));
   for(int i = 1; i <= 8; i++){
     map(&pcb->as, pcb->as.area.end - i * PGSIZE, _end + i * PGSIZE, 0x7);
   }
@@ -101,54 +101,54 @@ void context_uload(PCB* pcb, const char* filename, char *const argv[], char *con
 
   uintptr_t entry = loader(pcb, filename);
   
-  void* cur = _end;
+  // void* cur = _end;
   
-  int argc = 0, envc = 0;
-  // int arg_len = 0, env_len = 0;
-  while(argv && argv[argc]) argc ++;
-  while(envp && envp[envc]) envc ++;
-  // printf("argc: %d, envc: %d\n", argc, envc);
-  uintptr_t arg_p[argc];
-  uintptr_t env_p[envc];
-  printf("phase1\n");
-  int offset = sizeof(uintptr_t);
-  for(int i = 0; i < argc; i++){
-    int len = strlen(argv[i]);
-    strcpy(cur - offset - len - 1, argv[i]);
-    arg_p[i] = (uintptr_t)(cur - offset - len - 1);
-    // printf("%d %s %s %lx\n", argc, argv[i], (char*)(cur - offset - len - 1), cur - offset - len - 1);
-    offset += len + 1;
-  }
-  printf("phase2\n");
-  for(int j = 0; j < envc; j++){
-    int len = strlen(envp[j]);
-    strcpy(cur - offset - len - 1, envp[j]);
-    env_p[j] = (uintptr_t)(cur - offset - len - 1);
-    offset += len + 1;
-  }
-  printf("phase3\n");
-  *(uintptr_t*)(cur - offset - sizeof(uintptr_t)) = 0;
-  *(uintptr_t*)(cur - offset - sizeof(uintptr_t)*2) = 0;
-  offset += sizeof(uintptr_t) * 2;
-  printf("phase4\n");
-  for(int j = envc-1; j >= 0; j--){
-    *(uintptr_t*)(cur - offset - sizeof(uintptr_t)) = env_p[j];
-    offset += sizeof(uintptr_t);
-  }
-  *(uintptr_t*)(cur - offset - sizeof(uintptr_t)) = 0;
-  offset += sizeof(uintptr_t);
+  // int argc = 0, envc = 0;
+  // // int arg_len = 0, env_len = 0;
+  // while(argv && argv[argc]) argc ++;
+  // while(envp && envp[envc]) envc ++;
+  // // printf("argc: %d, envc: %d\n", argc, envc);
+  // uintptr_t arg_p[argc];
+  // uintptr_t env_p[envc];
+  
+  // int offset = sizeof(uintptr_t);
+  // for(int i = 0; i < argc; i++){
+  //   int len = strlen(argv[i]);
+  //   strcpy(cur - offset - len - 1, argv[i]);
+  //   arg_p[i] = (uintptr_t)(cur - offset - len - 1);
+  //   // printf("%d %s %s %lx\n", argc, argv[i], (char*)(cur - offset - len - 1), cur - offset - len - 1);
+  //   offset += len + 1;
+  // }
+  
+  // for(int j = 0; j < envc; j++){
+  //   int len = strlen(envp[j]);
+  //   strcpy(cur - offset - len - 1, envp[j]);
+  //   env_p[j] = (uintptr_t)(cur - offset - len - 1);
+  //   offset += len + 1;
+  // }
+  
+  // *(uintptr_t*)(cur - offset - sizeof(uintptr_t)) = 0;
+  // *(uintptr_t*)(cur - offset - sizeof(uintptr_t)*2) = 0;
+  // offset += sizeof(uintptr_t) * 2;
+  
+  // for(int j = envc-1; j >= 0; j--){
+  //   *(uintptr_t*)(cur - offset - sizeof(uintptr_t)) = env_p[j];
+  //   offset += sizeof(uintptr_t);
+  // }
+  // *(uintptr_t*)(cur - offset - sizeof(uintptr_t)) = 0;
+  // offset += sizeof(uintptr_t);
 
-  for(int i = argc-1; i >= 0; i--){
-    *(uintptr_t*)(cur - offset - sizeof(uintptr_t)) = arg_p[i];
-    offset += sizeof(uintptr_t);
-  }
-  printf("phase5\n");
-  *(uintptr_t*)(cur - offset - sizeof(uintptr_t)) = argc; 
-  offset += sizeof(uintptr_t);
+  // for(int i = argc-1; i >= 0; i--){
+  //   *(uintptr_t*)(cur - offset - sizeof(uintptr_t)) = arg_p[i];
+  //   offset += sizeof(uintptr_t);
+  // }
+  
+  // *(uintptr_t*)(cur - offset - sizeof(uintptr_t)) = argc; 
+  // offset += sizeof(uintptr_t);
 
   // printf("offset: %d %d\n", offset, sizeof(uintptr_t));
-  Area _area = {.start = pcb->as.area.start, .end = pcb->as.area.end - offset};
-  printf("phase6\n");
+  Area _area = {.start = pcb->as.area.start, .end = pcb->as.area.end };
+  
   Area _stack = {pcb->stack, pcb->stack + STACK_SIZE};
   pcb->cp = ucontext(&pcb->as, _stack, (void*)entry);
 
--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -9,18 +9,18 @@ extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
-  int argc = *args;
-  char** argv = (char**)(args + 1);
-  char** envp = (char**)(args + 1 + argc + 1);
+  // int argc = *args;
+  // char** argv = (char**)(args + 1);
+  // char** envp = (char**)(args + 1 + argc + 1);
 
-  printf("argc: %d\n", argc);
-  for(int i = 0; i < argc; i++){
-    printf("argv: %d %s %p\n", i, argv[i], &argv[i]);
-  }
-  for(int j = 0; envp[j]; j++) printf("envp: %d %s\n", j, envp[j]);
-  printf("end\n");
+  // printf("argc: %d\n", argc);
+  // for(int i = 0; i < argc; i++){
+  //   printf("argv: %d %s %p\n", i, argv[i], &argv[i]);
+  // }
+  // for(int j = 0; envp[j]; j++) printf("envp: %d %s\n", j, envp[j]);
+  // printf("end\n");
   
-  exit(main(argc, argv, envp));
-  // exit(main(0, empty, empty));
+  // exit(main(argc, argv, envp));
+  exit(main(0, empty, empty));
   assert(0);
 }
