1cd52cf009d7a14e8d59519b6ab9a8b8aa7434ed
tracer-ics2020 <tracer@njuics.org>
2021-05-23 14:28:34 +0800

    >  compile 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: 3383d3d361694b96806c5f00ad407121   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  14:28:34 up  6:39,  1 user,  load average: 0.73, 0.69, 0.72

--- nemu/src/isa/riscv64/mmu.c
+++ nemu/src/isa/riscv64/mmu.c
@@ -4,9 +4,9 @@
 #include "local-include/reg.h"
 
 #define VALID_MASK 1
-#define PGTABLE_MASK 0x3ffffffffffc00
+#define PGTABLE_MASK 0x3ffffffffffe00
 #define STAP_MASK 0xfffffffffff
-#define PG_OFFSET 0xfff
+#define PG_OFFSET 0x1ff
 
 paddr_t isa_mmu_translate(vaddr_t addr, int type, int len) {
   uint64_t* pg_base = (uintptr_t*)((reg_scr(SATP_ID) & STAP_MASK) << 12);
