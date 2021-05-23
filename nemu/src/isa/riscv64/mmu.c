93450362a47c93cc36479bf7177656bca99b53d3
tracer-ics2020 <tracer@njuics.org>
2021-05-23 14:47:46 +0800

    >  compile 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: 3383d3d361694b96806c5f00ad407121   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  14:47:46 up  6:59,  1 user,  load average: 0.76, 0.89, 0.86

--- nemu/src/isa/riscv64/mmu.c
+++ nemu/src/isa/riscv64/mmu.c
@@ -21,7 +21,7 @@ paddr_t isa_mmu_translate(vaddr_t addr, int type, int len) {
   // }
   assert(val & VALID_MASK);
 
-  pg_base = (uintptr_t*)((val >> 10) << 12);
+  pg_base = (uintptr_t*)(val & PGTABLE_MASK);
   idx = (addr >> 21) & 0x1ff;
   val = paddr_read((uintptr_t)(pg_base + idx), sizeof(uintptr_t));
   // printf("val2: %lx\n", val);
@@ -31,7 +31,7 @@ paddr_t isa_mmu_translate(vaddr_t addr, int type, int len) {
   // }
   assert(val & VALID_MASK);
 
-  pg_base = (uintptr_t*)((val & PGTABLE_MASK) << 2);
+  pg_base = (uintptr_t*)(val & PGTABLE_MASK);
   idx = (addr >> 12) & 0x1ff;
   val = paddr_read((uintptr_t)(pg_base + idx), sizeof(uintptr_t));
   // printf("val3: %lx\n", val);
@@ -41,7 +41,7 @@ paddr_t isa_mmu_translate(vaddr_t addr, int type, int len) {
   // }
   assert(val & VALID_MASK);
   
-  return ((val & PGTABLE_MASK) << 2) | (addr & PG_OFFSET);
+  return (val & PGTABLE_MASK)  | (addr & PG_OFFSET);
 }
 
 word_t vaddr_mmu_read(vaddr_t addr, int len, int type){
