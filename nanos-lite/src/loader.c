9f96e60ddf0ea45c92874219a3fcbe63bc2f01b2
tracer-ics2020 <tracer@njuics.org>
2021-05-23 11:15:44 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: 3383d3d361694b96806c5f00ad407121   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  11:15:44 up  3:26,  1 user,  load average: 0.74, 0.83, 0.81

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -51,7 +51,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
         // printf("loader read: %lx %lx %lx\n", (uintptr_t)_vaddr, (uintptr_t)PADDR_FROM_VADDR(_paddr, _vaddr), read_sz);
         if((uintptr_t)_vaddr == 0x40006000){
           uintptr_t* _tem = PADDR_FROM_VADDR(_paddr, _vaddr);
-          for(int i = 0; i < 0xc90; i+= sizeof(uintptr_t)) printf("i: %x %lx\n", i, _tem[i/sizeof(uintptr_t)]);
+          for(int i = 0; i < 0xc90; i+= sizeof(uintptr_t)) printf("i: %x %lx %lx\n", i, _tem[i/sizeof(uintptr_t)], (uintptr_t)PADDR_FROM_VADDR(_paddr, _vaddr) + i);
         }
         // *((uint8_t*)(_Pheader.p_vaddr + _offset)) = _data;
       } 
