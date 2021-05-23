2be3db1528f503a5522de8ee9dca92c819f559ac
tracer-ics2020 <tracer@njuics.org>
2021-05-23 12:21:23 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: 3383d3d361694b96806c5f00ad407121   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  12:21:23 up  4:32,  1 user,  load average: 1.24, 1.03, 0.89

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,7 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  // yield();
+  yield();
   const char* _p = buf;
   for(int i = 0; i < len; i++) putch(*_p++);
   return len;
