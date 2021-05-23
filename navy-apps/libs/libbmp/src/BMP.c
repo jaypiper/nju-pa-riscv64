811828d90e493dfa2be5d8632ead92d55d794504
tracer-ics2020 <tracer@njuics.org>
2021-05-23 15:44:20 +0800

    >  run 191220000    Static hostname: piper          Icon name: computer-laptop            Chassis: laptop         Machine ID: 704e20ce37a84f4db10747bad47a5c26            Boot ID: 3383d3d361694b96806c5f00ad407121   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  15:44:20 up  7:55,  1 user,  load average: 1.71, 1.32, 1.14

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -21,6 +21,7 @@ struct BitmapHeader {
 
 void* BMP_Load(const char *filename, int *width, int *height) {
   FILE *fp = fopen(filename, "r");
+  assert(fp);
   if (!fp) return NULL;
 
   struct BitmapHeader hdr;
