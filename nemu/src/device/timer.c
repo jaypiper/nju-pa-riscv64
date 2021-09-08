2c119e2afe18b0bdd5c333ea07d7192e3b9fa8ad
Chen Lu <181250012@smail.nju.edu.cn>
2021-09-08 21:06:37 +0800

    nemu,device: add clint

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -2,11 +2,19 @@
 #include <device/alarm.h>
 #include <monitor/monitor.h>
 #include <sys/time.h>
+#include <isa.h>
 
 #define RTC_PORT 0x48   // Note that this is not the standard
 #define RTC_MMIO 0xa1000048
+//ask the CLINT for a timer interrupt.
+#define CLINT                   (0x2000000L)
+#define CLINT_MTIMECMP(hartid)  (CLINT + 0x4000 + 4*(hartid))
+#define CLINT_MTIME             (CLINT + 0xBFF8)            // cycles since boot.
 
 static uint32_t *rtc_port_base = NULL;
+static uint8_t  *clint_port = NULL;
+static uint64_t  *clint_mtimecmp_port = NULL;
+static uint64_t  *clint_mtime_port = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
@@ -25,9 +33,25 @@ static void timer_intr() {
   }
 }
 
+static void timer_cmp(uint32_t offset, int len, bool is_write){
+
+}
+
+void timer_update(){
+  *clint_mtime_port = *clint_mtime_port + 1;
+  if(*clint_mtime_port > *clint_mtimecmp_port) cpu.INTR = 1;
+}
+
 void init_timer() {
   rtc_port_base = (void*)new_space(8);
   add_pio_map("rtc", RTC_PORT, (void *)rtc_port_base, 8, rtc_io_handler);
   add_mmio_map("rtc", RTC_MMIO, (void *)rtc_port_base, 8, rtc_io_handler);
   add_alarm_handle(timer_intr);
+
+  clint_port = new_space(1);
+  add_mmio_map("clint", CLINT, clint_port, 1, NULL);
+  clint_mtimecmp_port = (void*)new_space(0x8);
+  add_mmio_map("clint_mtimecmp", CLINT_MTIMECMP(0), (void*)clint_mtimecmp_port, 8, timer_cmp);
+  clint_mtime_port = (void*)new_space(8);
+  add_mmio_map("clint_mtime", CLINT_MTIME, (void*)clint_mtime_port, 8, timer_cmp);
 }
